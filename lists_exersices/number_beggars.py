07ad70ab663ee4cd4493c54ca4c920bb26090288
MladenAngelov-dev <123299807+MladenAngelov-dev@users.noreply.github.com>
2023-05-29 11:15:56 +0300

    tasks so far

--- lists_exersices/number_beggars.py
+++ lists_exersices/number_beggars.py
@@ -6,8 +6,13 @@ income_digits = []
 for symbol in income:
     income_digits.append(int(symbol))
 
-total_income =[]
+total_income = []
 start_index = 0
 
 while start_index < num_beggars:
-
+    current_beggar_sum = 0
+    for index in range(start_index, len(income_digits), num_beggars):
+        current_beggar_sum += income_digits[index]
+    total_income.append(current_beggar_sum)
+    start_index += 1
+print(total_income)
