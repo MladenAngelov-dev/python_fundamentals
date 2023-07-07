d41eee19611e492ba7c8c64a2491bee2681e09a1
MladenAngelov-dev <123299807+MladenAngelov-dev@users.noreply.github.com>
2023-07-07 13:48:59 +0300

    new week new tings to learn

--- string_formating/substring.py
+++ string_formating/substring.py
@@ -2,6 +2,5 @@ first_string = input()
 second_string = input()
 
 while first_string in second_string:
-    filtered_string = second_string.replace(first_string, "")
-    second_string = filtered_string
-print(filtered_string)
+    second_string = second_string.replace(first_string, "")
+print(second_string)
