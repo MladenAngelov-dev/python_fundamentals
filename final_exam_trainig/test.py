927f319b1bf584eb85582b301ab5755e5a28688c
MladenAngelov-dev <123299807+MladenAngelov-dev@users.noreply.github.com>
2023-07-13 21:26:20 +0300

    new week new tings to learn

---
+++ final_exam_trainig/test.py
@@ -0,0 +1,13 @@
+string = "asdba654sdasdadfsgd"
+start = 3
+end = 8
+new_string = "asd5"
+if new_string in string:
+    print(string)
+abcdefghijklmnopqrstuvwxyz
+Slice>>>2>>>6
+Flip>>>Upper>>>3>>>14
+Flip>>>Lower>>>5>>>7
+Contains>>>def
+Contains>>>deF
+Generate
