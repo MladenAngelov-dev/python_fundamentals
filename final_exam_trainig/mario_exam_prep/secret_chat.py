b90ff00cdf9b506c6862a0a21daaeebc68989d56
MladenAngelov-dev <123299807+MladenAngelov-dev@users.noreply.github.com>
2023-07-21 07:28:59 +0300

    tasks so far

--- final_exam_trainig/mario_exam_prep/secret_chat.py
+++ final_exam_trainig/mario_exam_prep/secret_chat.py
@@ -8,14 +8,23 @@ def decrypting_message(message):
             break
 
         elif command.startswith("InsertSpace"):
-            pass
+            current_command, index = command.split(":|:")
+            message = message[:int(index)] + " " + message[int(index):]
+            print(message)
 
         elif command.startswith("Reverse"):
-            pass
+            current_command, substring = command.split(":|:")
+            if substring in message:
+                message = message.replace(substring, '', 1)
+                message = message + substring[::-1]
+                print(message)
+            else:
+                print("error")
 
         elif command.startswith("ChangeAll"):
-            pass
-
+            current_command, substring, replacement = command.split(":|:")
+            message = message.replace(substring, replacement)
+            print(message)
 
 
 data = input()
